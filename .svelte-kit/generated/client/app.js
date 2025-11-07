6f7e285bee879322928b134a99d6b2f2ba6b5980
Anton Olsson <anton-olsson@outlook.com>
2025-11-07 20:29:05 +0100

    add vite sveltekit

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,35 @@
+import * as universal_hooks from '../../../src/hooks.js';
+
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [2],
+		"/demo": [3],
+		"/demo/paraglide": [4]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+	
+	reroute: universal_hooks.reroute || (() => {}),
+	transport: universal_hooks.transport || {}
+};
+
+export const decoders = Object.fromEntries(Object.entries(hooks.transport).map(([k, v]) => [k, v.decode]));
+export const encoders = Object.fromEntries(Object.entries(hooks.transport).map(([k, v]) => [k, v.encode]));
+
+export const hash = false;
+
+export const decode = (type, value) => decoders[type](value);
+
+export { default as root } from '../root.js';
---
+++ .svelte-kit/non-ambient.d.ts
@@ -0,0 +1,43 @@
+
+// this file is generated — do not edit it
+
+
+declare module "svelte/elements" {
+	export interface HTMLAttributes<T> {
+		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-preload-code'?:
+			| true
+			| ''
+			| 'eager'
+			| 'viewport'
+			| 'hover'
+			| 'tap'
+			| 'off'
+			| undefined
+			| null;
+		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
+		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
+	}
+}
+
+export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/" | "/demo" | "/demo/paraglide";
+		RouteParams(): {
+			
+		};
+		LayoutParams(): {
+			"/": Record<string, never>;
+			"/demo": Record<string, never>;
+			"/demo/paraglide": Record<string, never>
+		};
+		Pathname(): "/" | "/demo" | "/demo/" | "/demo/paraglide" | "/demo/paraglide/";
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): "/robots.txt" | string & {};
+	}
+}
